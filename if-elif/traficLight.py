e8eb159fb02f12286e555aa69d3ffb713e06589c
Ayush Kanoje <ayushkanoje056@gmail.com>
2025-11-09 22:33:39 +0530

    Trafic light system

---
+++ if-elif/traficLight.py
@@ -0,0 +1,10 @@
+light = input("Enter light colour:")
+
+if(light == "red" or light == "Red"):
+    print("Stop")
+elif(light == "yellow" or light == "Yellow"):
+    print("View And Get Ready")
+elif(light == "green" or light == "Green"):
+    print("Gooooo Buddy")
+else:
+    print("Light is Broken")       
