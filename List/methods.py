8a40ad18a94817ee8f8957aa3ca7e79b0bbe5697
Ayush Kanoje <ayushkanoje056@gmail.com>
2025-11-14 00:51:42 +0530

    added methods in method.py

---
+++ List/methods.py
@@ -0,0 +1,15 @@
+#1.Append- used to add an item to the end of the list.
+myList1 = [15,56,58,54,43,1,23,5,4]
+myList1.append(3)
+print(myList1)
+
+#2.Sort- Arrange values in ascending order.
+myList1.sort()
+print(myList1)
+#To arrange in decending order
+myList1.sort(reverse=True)
+print(myList1)
+#It also work for variablw values
+myList2 = ["anushka", "ayush", "akshad", "parikshita"]
+myList2.sort()
+print(myList2)
