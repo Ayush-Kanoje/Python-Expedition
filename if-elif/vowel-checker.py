ae4d207ce4bf1f9070d9be01abf3c9f11ebfc7ba
Ayush Kanoje <ayushkanoje056@gmail.com>
2025-11-13 19:25:48 +0530

    update vowel checkerr code

--- if-elif/vowel-checker.py
+++ if-elif/vowel-checker.py
@@ -1,8 +1,13 @@
-userInput = input("Enter anything i will be check if it is a vowel, number or special character")
+userInput = input("Enter anything i will be check if it is a vowel, number or special character: ")
 
-vowel = ['a','e','i','o','u']
-
-if(userInput >= "a" and userInput <= "z" or userInput >= "A" and userInput <= "Z"):
-    inputChanger = userInput.lower()
+if(userInput .isalpha()):
+    userInput = userInput.lower()
+    if(userInput == 'a' or userInput == 'e' or userInput == 'i' or userInput == 'o' or userInput =='u'):
+        print("It is a vowel", userInput)
+    else:
+        print("Its is not a vowel just a alphabet", userInput)  
+elif(userInput .isdigit()):
+    print("It is a number", userInput)
     
-    print()
+else:
+    print("It is a special character:", userInput)
