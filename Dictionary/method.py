b9455e17660f5ad226fc04eba984272f495934e0
Ayush Kanoje <ayushkanoje056@gmail.com>
2025-11-21 10:29:42 +0530

    added new methods in dictionary

--- Dictionary/method.py
+++ Dictionary/method.py
@@ -8,14 +8,28 @@ student = {
     },
     "cgpa" : 9.5,
 }
+print(student)
 
-#1. TO PRINT ALL THE KEY INSIDE OF DICT.
+#1 TO PRINT ALL THE KEY INSIDE OF DICT.
 print(student.keys()) #nested dict key:value are not returned
+print(student["subject"].keys())
 
-#2. TO FIND LEN OF DICT
+#2 TO PRINT ALL THE VALUES INSIDE OF A DICT
+print(student.values())
+print(student["subject"].values())
+
+#3 TO FIND LEN OF DICT
 print(len(student)) #count inner dict as one key:value
 print(len(student["subject"])) #inner dict len
 
-#3. TO TYPE CAST
+#4 TO TYPE CAST
 print(list(student))
+print(list(student["subject"]))
+
+#5 .itmes() -> IT RETURN VIEW OF A DICT IN OBJECT CONATINING KEY:VALUE PAIR AS A TOUPLES
+
+print(student.items())
+
+list_cating = list(student.items()) #converting into list
+print(list_cating)
 
