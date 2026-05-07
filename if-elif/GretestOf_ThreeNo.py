2c109960f9e0f9c6791f4eef8021675d507e4cf2
Ayush Kanoje <ayushkanoje056@gmail.com>
2026-05-07 16:32:43 +0530

    added new code with real world use case of how password system work

--- if-elif/GretestOf_ThreeNo.py
+++ if-elif/GretestOf_ThreeNo.py
@@ -1,15 +1,34 @@
-num1 = int(input("Enter First No:"))
-num2 = int(input("Enter Second No:"))
-num3 = int(input("Enter Third No:"))
+# num1 = int(input("Enter First No:"))
+# num2 = int(input("Enter Second No:"))
+# num3 = int(input("Enter Third No:"))
 
 
-if(num1>num2 and num1>num3):
-    print("No 1 is greater")
+# if(num1>num2 and num1>num3):
+#     print("No 1 is greater")
+
+# elif(num2>num1 and num2>num3):
+#     print("No 2 is greater")
+
+# elif(num3>num1 and num3>num2):
+#     print("No 3 is greater")
+# else:
+#     print("You have entered wrong no or same no")
+
+
+
+#code if user enter duplicate number
+num1 = int(input("Enter your first number: "))
+num2 = int(input("Enter your second number: "))
+num3 = int(input("Enter your third number: "))
+
+if(num1 == num2 or num1 == num3 or num2 == num3):
+    print("You have entered duplicate no")
+
+elif(num1>num2 and num1>num3):
+    print(f"First number is greter: {num1}")   
 
 elif(num2>num1 and num2>num3):
-    print("No 2 is greater")
+        print(f"Second number is greater: {num2}")
 
-elif(num3>num1 and num3>num2):
-    print("No 3 is greater")
 else:
-    print("You have entered wrong no or same no")
+    print(f"Third numeber is greater: {num3}")        
---
+++ if-elif/password-checker.py
@@ -0,0 +1,7 @@
+password = input("Enter your password: ")
+comform_password = input("Comform your password: ")
+
+if(password == comform_password):
+    print("Password comformed, You can enter in my infinite void")
+else:
+    print("Entered password is incorrect, Please enter correct password")    
