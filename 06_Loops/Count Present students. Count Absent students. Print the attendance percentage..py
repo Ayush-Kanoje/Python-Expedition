8a42840d78a6373297fc6e7baac379f6bb770936
Ayush Kanoje <ayushkanoje056@gmail.com>
2026-07-02 13:16:20 +0530

    added new code

---
+++ 06_Loops/Count Present students. Count Absent students. Print the attendance percentage..py
@@ -0,0 +1,35 @@
+
+# Count Present students.
+# Count Absent students.
+# Print the attendance percentage.
+# Present : 6
+# Absent : 4
+# Attendance : 60%
+# The total number of students is the length of the list
+
+
+
+
+attendance = [
+    "Present",
+    "Absent",
+    "Present",
+    "Present",
+    "Absent",
+    "Present",
+    "Present",
+    "Absent",
+    "Present",
+    "Absent"
+]
+
+count_present = 0
+count_absent = 0
+
+for presenty in attendance:
+    if presenty == "Present":
+        count_present += 1
+    else:
+        count_absent += 1
+print(f"Total present student: {count_present}\nTotal Absent Student: {count_absent}")
+print(count_present/len(attendance)*100)
