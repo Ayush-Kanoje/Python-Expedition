916f4733cd1eaff35704283f92f9a65ce467d6da
Ayush Kanoje <ayushkanoje056@gmail.com>
2026-07-02 16:31:22 +0530

    added valid input checker code

--- 06_Loops/valid_input_analyzer.py
+++ 06_Loops/valid_input_analyzer.py
@@ -1,1 +1,7 @@
-
+while True:
+    user = int(input("Enter no between 1 to 10: "))
+    if user >= 0 and user <= 10:
+        print("Access Appovied")
+        break
+    else:
+        print("Access Denied")
