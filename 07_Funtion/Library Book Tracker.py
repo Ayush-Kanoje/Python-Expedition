0016104e54dea281dd1714524836e1a5c44f3af7
Ayush Kanoje <ayushkanoje056@gmail.com>
2026-07-03 00:57:07 +0530

    removed library code

--- 07_Funtion/Library Book Tracker.py
+++ 07_Funtion/Library Book Tracker.py
@@ -4,20 +4,7 @@
 
 # Finally, your program should display:
 
-# Total books borrowed.
-# All book titles.
-# The first book entered.
-# The last book entered.
-# User Input Example
-# How many books were borrowed today?
-# 4
-
-# Enter book 1: Python Basics
-# Enter book 2: Atomic Habits
-# Enter book 3: Clean Code
-# Enter book 4: Deep Work
-# Expected Output
-# Today's Library Report
+# 📚 Today's Library Report
 
 # Total Books Borrowed : 4
 
@@ -28,18 +15,4 @@
 # 4. Deep Work
 
 # First Book : Python Basics
-# Last Book : Deep Work
-# Rules
-# Ask the user for the number of books.
-# Use a loop.
-# Store the book names in a list.
-
-
-total_no_of_books_borrowed = int(input("Enter total count of borrowed books: "))
-
-def books(total_no_of_books_borrowed):
-    for books_title in range(1,total_no_of_books_borrowed):
-        books_list = input("Enter books title: ").append(books_list)
-    print(books(books_list))
-
-print(books(total_no_of_books_borrowed))
+# Last Book  : Deep Work
