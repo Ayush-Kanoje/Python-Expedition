51ad054aced20daf4468bf95cd5a32a4ace47ef3
Ayush Kanoje <ayushkanoje056@gmail.com>
2026-07-03 16:58:37 +0530

    library report code uploaded

--- 07_Funtion/Library Book Tracker.py
+++ 07_Funtion/Library Book Tracker.py
@@ -16,3 +16,19 @@
 
 # First Book : Python Basics
 # Last Book  : Deep Work
+
+
+user_input = int(input("Enter total no of books borrowed: "))
+def library_report(user_input):
+    books_tiles = []
+    for i in range(user_input):
+        books_tiles.append(input("Enter title of books: "))
+    print("Today Books report of borrowed books\n")
+    print("Number of books borrowed: ", user_input)
+    for i, books in enumerate(books_tiles, 1):
+        print(f"\nBooks Borrowed: {i}. {books}")
+
+    print(f"\nFirst Borrowed book: {books_tiles[0]}\nSecond Borrowed book: {books_tiles[-1]}")
+    
+
+library_report(user_input)
