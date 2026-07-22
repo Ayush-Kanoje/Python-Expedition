5b5b5bf9e722b97732c2355adcd832a4946e00a2
Ayush Kanoje <ayushkanoje056@gmail.com>
2026-07-22 17:11:38 +0530

    Add comments and new list slicing example

--- 02_List/methods.py
+++ 02_List/methods.py
@@ -1,39 +1,41 @@
 #1.Append- used to add an item to the end of the list.
 myList1 = [1,2,3,4,5,6,7,8,9,10,2,11,2]
 myList1.append(3)
-print(myList1)
+print(myList1, "\n")
 
 #2 .extend() -> use to concat two list, can be used with set,touples,string
 a = ['a','b','c']
 b = ['d','e']
 a.extend(b)
-print(a)
+print(a, "\n")
 
 #3.Sort- Arrange values in ascending order.
 myList1.sort()
-print(myList1)
+print(myList1, "\n")
 
 #To arrange in decending order
 myList1.sort(reverse=True)
-print(myList1)
+print(myList1, "\n")
 
 #It also work for variable
 myList2 = ["anushka", "ayush", "akshad", "parikshita"]
 myList2.sort()
-print(myList2)
+print(myList2, "\n")
 
 #Reverse- reverse all the value present inside list (does not sort the list)
 myList2.reverse()
-print(myList2)
+print(myList2, "\n")
 
 #Insert- Used to insert values at any index of a list
 myList2.insert(0, "jesus")
-print(myList2)
+print(myList2, "\n")
 
 #Remove- remove first occurance of any element
 myList1.remove(2)
-print(myList1)
+print(myList1, "\n")
 
 #Pop- used to removed values using index position
 myList2.pop(2)
-print(myList2)
+print(myList2, "\n")
+
+
