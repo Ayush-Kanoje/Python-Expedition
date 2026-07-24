11d51e9989807244eb7478891a133de49da6894d
Ayush Kanoje <ayushkanoje056@gmail.com>
2026-07-24 22:20:38 +0530

    solved a question based on real world use case of aws

--- 02_List/practice_que04.py
+++ 02_List/practice_que04.py
@@ -10,24 +10,28 @@
 # Print every high usage value.
 # Find highest CPU usage.
 # Calculate average CPU usage.
-# Print "Healthy" if average < 70, otherwise print "Needs Investigation".
+# Print "Healthy" if average < 80, otherwise print "Needs Investigation".
 
 
 cpu_usage = [30, 45, 82, 91, 75, 68, 95, 40]
 
-count_useage = 0
-
-
 def cpu_usage_cal(cpu_usage):
-    for i in range(len(cpu_usage)):
+    count_useage = 0
+    for i in range(len(cpu_usage)): #CPU useage count krega 
         if cpu_usage[i] > 80:
             count_useage += 1
-            print(f"CPU Useage Exceed {count_useage}: {cpu_usage[i]}")
+            print(f"CPU Useage Exceed {count_useage}: {cpu_usage[i]} - Needs Investigation")
             continue
-    print(cpu_usage.high())
+        else:
+            print(f"Healthy: {cpu_usage[i]}")
+    
+    print(f"Highest CPU usage {max(cpu_usage)}")
+
+    avg_cpu_useage = sum(cpu_usage)/len(cpu_usage)
+    print(avg_cpu_useage)
 
 
-
+cpu_usage_cal(cpu_usage)
 
 
 
